871e99d4e22a41610df40167d46439aac2b01125
Ahmad <yusufadinata19@gmail.com>
2019-10-04 06:54:33 +0700

    ubah gambar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,42 @@
-import React, { Component } from 'react';
-import user from './asset/user.jpeg';
+import React, { Component } from "react";
+import user from "./asset/avatar.jpg";
 
 class Header extends Component {
-    showStatus = () => {
-        this.props.show()
-    }
+  showStatus = () => {
+    this.props.show();
+  };
 
-    render() {
-        return (
-            <div className="header">
-                <div className="find">
-                    <i className="demo-icon icon-search">&#xe806;</i>
-                    <input type="text" placeholder="Find a user, team, meeeting..."/>
-                    
-                </div>
-                <div className="user">
-                    <span className="tombol" href="#"><i className="demo-icon icon-paper-plane">&#xe80b;</i> Invite</span>
-                    <img src={user} alt="user"/>
-                    {
-                        this.props.style === "show-status" ? 
-                        <i onClick={this.showStatus} className="demo-icon toogle icon-cancel-1">&#xe80c;</i> :
-                        <i onClick={this.showStatus} className="demo-icon toogle icon-th-list">&#xe80a;</i>
-                    }
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header">
+        <div className="find">
+          <i className="demo-icon icon-search">&#xe806;</i>
+          <input type="text" placeholder="Find a user, team, meeeting..." />
+        </div>
+        <div className="user">
+          <span className="tombol" href="#">
+            <i className="demo-icon icon-paper-plane">&#xe80b;</i> Invite
+          </span>
+          <img src={user} alt="user" />
+          {this.props.style === "show-status" ? (
+            <i
+              onClick={this.showStatus}
+              className="demo-icon toogle icon-cancel-1"
+            >
+              &#xe80c;
+            </i>
+          ) : (
+            <i
+              onClick={this.showStatus}
+              className="demo-icon toogle icon-th-list"
+            >
+              &#xe80a;
+            </i>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
